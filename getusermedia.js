3d6ccc8d544998795c85378ae0f1119c6d778ed4
Marina Kukso <marina.kukso@gmail.com>
2017-06-20 17:15:26 +0200

    removed semicolons

--- getusermedia.js
+++ getusermedia.js
@@ -1,17 +1,16 @@
 var AudioContext = window.AudioContext || window.webkitAudioContext
 var audioCtx = new AudioContext()
-var analyser = audioCtx.createAnalyser();
+var analyser = audioCtx.createAnalyser()
 var getUserMedia = require('getusermedia')
  
 getUserMedia({audio: true}, function (err, stream) {
   if (err) {
      console.log('failed')
   } 
-  else source = audioCtx.createMediaStreamSource(stream);
-  source.connect(analyser);
-  analyser.fftSize = 2048;
-  var bufferLength = analyser.frequencyBinCount;
-  var dataArray = new Float32Array(bufferLength);
-  analyser.getFloatFrequencyData(dataArray);
+  else source = audioCtx.createMediaStreamSource(stream)
+  source.connect(analyser)
+  analyser.fftSize = 2048
+  var dataArray = new Float32Array(analyser.frequencyBinCount)
+  analyser.getFloatFrequencyData(dataArray)
   console.log(dataArray)
 });
