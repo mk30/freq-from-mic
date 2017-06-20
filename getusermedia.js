7d852e22b4cb92dc97cc330cc8257f660e9e5be0
Marina Kukso <marina.kukso@gmail.com>
2017-06-20 20:12:36 +0200

    now getting frequencies & their amplitudes out of mic

--- getusermedia.js
+++ getusermedia.js
@@ -1,6 +1,9 @@
 var AudioContext = window.AudioContext || window.webkitAudioContext
 var context = new AudioContext()
 var getUserMedia = require('getusermedia')
+var ndarray = require('ndarray')
+var fft = require('ndarray-fft')
+var mag = require('ndarray-complex').mag
  
 getUserMedia({audio: true}, function (err, stream) {
   if (err) {
@@ -10,7 +13,21 @@ getUserMedia({audio: true}, function (err, stream) {
   var processor = context.createScriptProcessor(1024,1,1);
   input.connect(processor);
   processor.connect(context.destination);
-  processor.onaudioprocess = function(e){
-    console.log(e.inputBuffer);
+  var arr = new Float32Array(1024)
+  processor.onaudioprocess = function(ev){
+    var data = ev.inputBuffer.getChannelData(0)
+    var reals = ndarray(data,[data.length,1])
+    var imags = ndarray(arr,[data.length,1])
+    for (var i = 0; i < 1024; i++){
+      arr[i] = 0
+    }
+    fft(1,reals,imags)
+    mag(reals,reals,imags)
+    for (var i=0;i<reals.data.length/2;i++){
+      var freq = i * context.sampleRate / reals.data.length  
+      if (reals.data[i]>3000){
+        console.log(freq, reals.data[i])
+      }
+    }
   };
 });
